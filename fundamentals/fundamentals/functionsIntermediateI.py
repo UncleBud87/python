4b3e4d31d374f2f2a572277882d240dbc4e0a3e1
UncleBud87 <lperioux@gmail.com>
2022-02-07 12:47:27 -0600

    commit_1

--- fundamentals/fundamentals/functionsIntermediateI.py
+++ fundamentals/fundamentals/functionsIntermediateI.py
@@ -1,3 +1,6 @@
+from re import A
+
+
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -9,8 +12,18 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
+x = [ [5,2,3], [10,8,9] ] 
+def changeList(x):
+    x = [[5,2,3],[15,8,9]]
+    return (x)
+changeList( x )
+print( x )
+
+#//////////////////////
+
+
 students = [
-        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'Michael', 'last_name' : 'Jordan'},
         {'first_name' : 'John', 'last_name' : 'Rosales'},
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
@@ -18,20 +31,28 @@ students = [
 iterateDictionary(students) 
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
-Michael
-John
-Mark
-KB
 
-Jordan
-Rosales
-Guillen
-Tonel
+#//////////////////////
+
+
+# Michael
+# John
+# Mark
+# KB
+
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+
+#//////////////////////
+
 
 dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],

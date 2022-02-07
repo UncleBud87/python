f137d421409e6adac079b3dd42f10c0df0fe3434
UncleBud87 <lperioux@gmail.com>
2022-02-07 11:17:39 -0600

    commit_1

--- fundamentals/fundamentals/functionsBasicII.py
+++ fundamentals/fundamentals/functionsBasicII.py
@@ -1,6 +1,49 @@
 
-def countDown():
-    for x in range(10,0,-1):
-        print (x)
+def countDown(num):
+    output = []
+    for x in range(num,-1,-1):
+        output.append(x)
+    return output
 
-countDown
+print(countDown(5))
+
+#///////////////////////////
+
+def printAndReturn(list):
+    print(list[0])
+    return list[1]
+
+print(printAndReturn([1,2]))
+
+#//////////////////////////
+
+def first_plus_length(list):
+    return list[0] + len(list)
+
+print(first_plus_length([1,2,3,4,5]))
+
+#//////////////////////////
+
+def values_greater_than_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for i in range(0,len(list)):
+        if list[i] > list[1]:
+            output.append(list[i])
+    print(len(output))
+    return output
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([3]))
+
+#//////////////////////////
+
+def length_and_value(size,value):
+    output = []
+    for i in range(0, size):
+        output.append(value)
+    return output
+
+print(length_and_value(4,7))
+print(length_and_value(6,2))

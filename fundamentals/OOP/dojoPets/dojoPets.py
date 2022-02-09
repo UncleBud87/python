8b3ce5ec96c927854d93691100dd6af9a6400297
UncleBud87 <lperioux@gmail.com>
2022-02-09 17:09:32 -0600

    commit_1

--- fundamentals/OOP/dojoPets/dojoPets.py
+++ fundamentals/OOP/dojoPets/dojoPets.py
@@ -1,8 +1,40 @@
 
-from pets import *
+class Pet:
+
+    def __init__(self, name, type, tricks):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.energy = 100
+        self.health = 100
+
+    def sleep(self):
+        self.energy += 25
+        print(f"{self.name} sleeps, ENERGY: {self.energy}")
+        return self
+
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        print(f"{self.name} eats, ENERGY: {self.energy}")
+        return self
+
+    def play(self):
+        self.health += 5
+        self.energy -= 5
+        print(f"{self.name} plays, HEALTH: {self.health} ENERGY: {self.energy}")
+        return self
+
+    def noise(self):
+        print("woof")
+
+    def display(self):
+        print(
+            f'name: {self.name}, type: {self.type}, tricks: {self.energy}, health: {self.health}')
+        return self
 
 
-class Ninja():
+class Ninja:
 
     def __init__(self, first_name, last_name, treats, pet_food, pet):
         self.first_name = first_name
@@ -13,23 +45,24 @@ class Ninja():
 
     def walk(self):
         self.pet.play()
+        self.pet.display()
         return self
 
     def feed(self):
         self.pet.eat()
+        self.pet.display()
         return self
 
     def bathe(self):
         self.pet.noise()
+        self.pet.display()
         return self
 
 
-john = Ninja("John", "Smith", "Maverick", "Bacon", "Merrick")
-maverick = Pet("Maverick", "Aussy", "Jump", 90, 80)
 
-john.walk()
+maverick = Pet("Maverick", "Aussy", "Jump")
+john = Ninja("John", "Smith", "bacon", "merrick", maverick)
 
-# sleep() - increases the pets energy by 25
-# eat() - increases the pet's energy by 5 & health by 10
-# play() - increases the pet's health by 5
-# noise() - prints out the pet's sound
+john.walk().feed().bathe()
+
+# maverick.sleep().display().eat().display().play().display().noise()
--- fundamentals/OOP/dojoPets/pets.py
+++ fundamentals/OOP/dojoPets/pets.py
@@ -1,21 +1,21 @@
 
-class Pet():
+class Pet:
 
-    def __init__(self, name, type, tricks, energy, health):
-        self.pet = name
+    def __init__(self, name, type, tricks):
+        self.name = name
         self.type = type
         self.tricks = tricks
-        self.energy = energy
-        self.health = health
+        self.energy = 100
+        self.health = 100
 
     def sleep(self):
-        self.energy = + 25
+        self.energy += 25
         print(f"{self.name} sleeps, ENERGY: {self.energy}")
         return self
 
     def eat(self):
         self.energy += 5
-        self.health += +10
+        self.health += 10
         print(f"{self.name} eats, ENERGY: {self.energy}")
         return self
 
@@ -28,4 +28,41 @@ class Pet():
     def noise(self):
         print("woof")
 
+    def display(self):
+        print(
+            f'name: {self.name}, type: {self.type}, tricks: {self.energy}, health: {self.health}')
+        return self
 
+
+class Ninja:
+
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+
+    def walk(self):
+        self.pet.play()
+        self.pet.display()
+        return self
+
+    def feed(self):
+        self.pet.eat()
+        self.pet.display()
+        return self
+
+    def bathe(self):
+        self.pet.noise()
+        self.pet.display()
+        return self
+
+
+
+maverick = Pet("Maverick", "Aussy", "Jump")
+john = Ninja("John", "Smith", "bacon", "merrick", maverick)
+
+john.walk().feed().bathe()
+
+# maverick.sleep().display().eat().display().play().display().noise()

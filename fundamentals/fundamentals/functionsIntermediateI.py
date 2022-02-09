45aaa8bc1a52633b73e268fe52eeb4012acd9b83
UncleBud87 <lperioux@gmail.com>
2022-02-09 10:16:35 -0600

    commit_1

--- fundamentals/fundamentals/functionsIntermediateI.py
+++ fundamentals/fundamentals/functionsIntermediateI.py
@@ -53,20 +53,17 @@ iterateDictionary(students)
 # first_name - KB, last_name - Tonel
 
 students = [
-        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name': 'Michael', 'last_name' : 'Jordan'},
         {'first_name' : 'John', 'last_name' : 'Rosales'},
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-def iterateDictionary2(a, b):
-    for x in range(0,4):
-        if str(a) == 'first_name':
-            print(students[x]['first_name'])
-        elif str(a) == 'last_name':
-            print(students[x]['last_name'])
+def iterateDictionary2(key_name, some_list):
+    for i in range(0,len(some_list)):
+        print(some_list[i][key_name])
 
-print(iterateDictionary2('first_name', students))
-print(iterateDictionary2('last_name', students))
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
 
 #Michael
 #John

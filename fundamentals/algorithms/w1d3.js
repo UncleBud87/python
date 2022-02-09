1bfc713129872f9b6d48d739d5a5941e4f729e0b
UncleBud87 <lperioux@gmail.com>
2022-02-09 10:31:14 -0600

    commit_1

--- fundamentals/algorithms/w1d3.js
+++ fundamentals/algorithms/w1d3.js
@@ -15,6 +15,7 @@ function bubbleSort(arr) {
     }
 }
 
+
 let test_a = [8, 1, 4, 5, 9, 2, 7, 6, 3];
 bubbleSort(test_a);
 console.log(test_a); // should display [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -24,7 +25,47 @@ let test_b = [1, 3, 2, 3, 6, 3, 3, 3, 3, 3, 5, 9, 3];
 bubbleSort(test_b);
 console.log(test_b); // should display [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 5, 6, 9]
 
+
+let test_c = [1, 2, 3, 4, 5, 6, 7, 10, 1, 9];
+bubbleSort(test_c);
+console.log(test_c); // should display [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 5, 6, 9]
+
+
+let test_c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+bubbleSort(test_c);
+console.log(test_c); // should display [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 5, 6, 9]
+
 // let x = [2, 1, 3, 4, 5];
 // let temp = x[0];
 // x[0] = x[1];
 // x[1] = temp;
+
+
+
+// extra
+
+function bubbleSort(arr) {
+
+    let ops = 0;
+
+
+    for (var j = 0; j < arr.length; j++) {
+        ops++;
+        let swapped = false;
+        for (i = j + 1; i < arr.length; i++) {
+            ops++;
+            if (arr[j] > arr[i]) {
+                ops++;
+                let temp = arr[j]; // [arr[i], arr[j]] = [arr[j], arr[i]]
+                arr[j] = arr[i];
+                arr[i] = temp;
+                swapped = true;
+            }
+        }
+        if (swapped == false) {
+            console.log(ops);
+            return undefined;
+        }
+    }
+    console.log(ops);
+}

0b52be5420568d2aa944280db75e104e78592797
UncleBud87 <lperioux@gmail.com>
2022-02-13 15:56:09 -0600

    commit_1

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'look a secret'
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def counter():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] += 1
+    return render_template('index.html')
+
+@app.route('/destroy')
+def destroy():
+    session.pop("count")
+    return redirect('/')
+
+@app.route('/add')
+def add():
+    session["count"] += 1
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)

5f93dc6dcc2804d0e2dc0dfd59d8c9304096db66
UncleBud87 <lperioux@gmail.com>
2022-02-17 10:04:55 -0600

    commit_1

--- fundamentals/algorithms/w2d4.js
+++ fundamentals/algorithms/w2d4.js
@@ -7,7 +7,34 @@
 // "race car" is not (the space doesn't match up with the E on the opposite side)
 
 function isPalindrome(str) {
-
+    for (var i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// what are our test cases?
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("tacocat"));
+console.log(isPalindrome("race car"));
+console.log(isPalindrome("e racecar e"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome(""));
+console.log(isPalindrome("a"));
+console.log(isPalindrome("123454321"));
+console.log(isPalindrome(" "));
+console.log(isPalindrome("1395742931"));
+
+
+//function isPalindrome(str) {
+//    for (var i = 0; i < str.length / 2; i++) {
+//        if (str[i] == str[str.length - 1 - i]) {
+//            continue;
+//        }
+//        else {
+//            return false;
+//        }
+//    }
+//    return true;
+//}

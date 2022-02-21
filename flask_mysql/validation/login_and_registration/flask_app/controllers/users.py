36760f4e217d5dcff8c3333f604a80afb4a02632
UncleBud87 <lperioux@gmail.com>
2022-02-21 13:02:38 -0600

    commit_1

--- flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
+++ flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask import render_template,redirect,request,session
+from flask import render_template,redirect,request,session, url_for,flash
 from flask_app import app
 from flask_app.models.user import User
 
@@ -7,8 +7,18 @@ from flask_app.models.user import User
 def index():
     return redirect('/login')
 
-
-
 @app.route("/login")
 def login():
     return render_template("login.html", users=User.get_all())
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
--- flask_mysql/validation/login_and_registration/flask_app/models/user.py
+++ flask_mysql/validation/login_and_registration/flask_app/models/user.py
@@ -9,20 +9,22 @@ class User:
         self.last_name = data['last_name']
         self.email = data['email']
         self.password = data['password']
+        self.confirm_password = data['confirm_password']
 
     @staticmethod
     def validate_user(user):
         is_valid = True # we assume this is true
+
         if len(user['first_name']) < 3:
             flash("First Name must be at least 3 characters.")
             is_valid = False
         if len(user['last_name']) < 3:
             flash("Last Name must be at least 3 characters.")
             is_valid = False
-        if len(user['email']) < 1:
-            flash("Calories must be 200 or greater.")
+        if len(user['email']) < 8:
+            flash("email must me longer than 8 characters.")
             is_valid = False
-        if len(user['meat']) < 3:
-            flash("Bun must be at least 3 characters.")
+        if len(user['password']) != len(user['confirm_password']):
+            flash("Password does not match.")
             is_valid = False
         return is_valid

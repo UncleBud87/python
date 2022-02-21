793f48bc2a54aee2ca65dd9b5cece7aa684906d0
UncleBud87 <lperioux@gmail.com>
2022-02-21 17:55:56 -0600

    commit_1

--- flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
+++ flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
@@ -1,42 +1,29 @@
 from flask import render_template,redirect,request,session, url_for,flash
-from flask_app import app
+from flask_app.__init__ import app
 from flask_app.models.user import User
 
 
 @app.route("/")
 def index():
-    return redirect('/login')
+    return render_template('index.html')
 
-@app.route("/login")
-def login():
-    return render_template("index.html")
+@app.route("/home")
+def home_page():
+    return render_template("home.html",first_name = session['first_name'])
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
-
-@app.route("/new/registration")
-def save():
-    User.save(request.form)
-    return redirect('/welcome')
-
-@app.route('/create', methods=['POST'])
-def create_account():
+@app.route('/new_user', methods=['POST'])
+def new_user():
     if not User.validate_account(request.form):
         return redirect('/')
 
-    pw_hash = bcrypt.generate_password_has(request.form['password'])
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
     data = {
         "first_name": request.form['first_name'],
-        'last_name': request.form['last_name'],
-        'email': request.form['email'],
-        'password': pw_hash
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": pw_hash
     }
-    account_in_db = User.get_by_email(request.form['email'])
-    if account_in_db:
-        flash('Email is already in use','error')
-        return redirect('/')
-    
+
     account_id = User.save(data)
     print('+'*20)
     print(account_id)
@@ -44,14 +31,24 @@ def create_account():
     session['login'] = True
     return redirect('/home')
 
-@app.route('/{{email}}/login', methods=['GET', 'POST'])
-def new_login():
-    error = None
-    if request.method == 'POST':
-        if request.form['email'] != 'email' or \
-                request.form['password'] != 'password':
-            error = 'Invalid credentials'
-        else:
-            flash('You were successfully logged in')
-            return redirect(url_for('index'))
-    return render_template('welcome.html', error=error)
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = {'email': request.form['email']}
+    account_in_db = User.get_by_email(data)
+
+    if not account_in_db:
+        flash('Invalid Email/Password', 'bad')
+        return redirect('/')
+    if not bcrypt.check_password_has(account_in_db.password,request.form['password']):
+        flash("Invalid Email/Password", 'bad')
+        return redirect('/')
+    session['first_name'] = account_in_db.first_name
+    session['login'] = True
+
+    return redirect('/home')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return redirect('/')
--- flask_mysql/validation/login_and_registration/flask_app/models/user.py
+++ flask_mysql/validation/login_and_registration/flask_app/models/user.py
@@ -1,4 +1,4 @@
-from pickle import FALSE
+
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 
@@ -13,43 +13,49 @@ class User:
         self.password = data['password']
         self.confirm_password = data['confirm_password']
 
-    @staticmethod
-    def validate_user(user):
-        is_valid = True # we assume this is true
+        self.account = []
 
-        if len(user['first_name']) < 2:
-            flash("First Name must be at least 2 characters.")
-            is_valid = False
-        if len(user['last_name']) < 2:
-            flash("Last Name must be at least 2 characters.")
-            is_valid = False
-        if User.get_by_email(user) != False:
-            flash('Email already in use', 'error')
-            is_valid = False
-        if user['password'] != user['confirm_password']:
-            flash("Password does not match.")
-            is_valid = False
-        return is_valid
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO user_registration (first_name, last_name, email, password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s;"
+        return connectToMySQL('login_registration').query_db(query,data)
+
+    @classmethod
+    def get_by_id(cls,data):
+        query = "select * FROM user_registration WHERE id = %(id)s"
+        return connectToMySQL('login_registration').query_db(query,data)
 
     @classmethod
     def get_by_email(cls,data):
         query = "SELECT * FROM user_registration WHERE email = %(email)s;"
-        result = connectToMySQL('user_registration_schema').query_db(query,data)
+        result = connectToMySQL('login_registration').query_db(query,data)
+
         if len(result) < 1:
             return False
         return cls(result[0])
 
-    @classmethod
-    def get_all(cls):
-        query = "SELECT * FROM users;"
-        results = connectToMySQL('users_schema').query_db(query)
-        users = []
-        for u in results:
-            users.append( cls(u) )
-        return users
+    @staticmethod
+    def validate_account(account):
+        is_valid = True
+        if len(account['first_name']) < 2:
+            flash("First Name must be atleast 2 characters", 'error')
+            is_valid = False
+        if len(account['last_name']) < 2:
+            flash("Last Name must be atleast 2 characters", 'error')
+            is_valid = False
+        if not EMAIL_REGEX.match(account['email']):
+            flash('Invalid email address!', 'error')
+            is_valid = False
+        if Regisrtr.get_by_email(account) != False :
+            flash('Email already in use', 'error')
+            is_valid = False
+        if not PASSWORD_REGEX.match(account['password']):
+            flash('Password cannot be empty and needs atleast one uppercase letter, one lowercase letter, and one number(MIN of 8 characters ) ', 'error')
+            is_valid = False
+        if account['password'] != account['confirm_password']:
+            flash("Passwords do not match!", 'error')
+            is_valid = False
+        
 
-    @classmethod
-    def save(cls, data):
-        query = "INSERT INTO user_registration (first_name,last_name,email,password,confirm_password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s,%(confirm_password)s);"
-        result = connectToMySQL('users_registration_schema').query_db(query,data)
-        return result
+        return is_valid
+
